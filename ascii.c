54768e7fa5282e545d7dd92c52c91e09b4a39a77
Vishnu Anup <vishnuanup06@gmail.com>
2024-10-14 11:55:07 +0530

    Add files via upload

---
+++ ascii.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	char a;
+	printf("Enter a character to display the corresponding ASCII value: ");
+	scanf("%c",&a);
+	printf("ASCII value of %c = %d",a,a);
+	return 0;
+}
