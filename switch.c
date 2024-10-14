0b8e148f3728ecefc6ff9f5a7b819cf3f8b54e2d
vishnuv1z <vishnuanup06@gmail.com>
2024-10-14 11:49:07 +0530

    Add files via upload

---
+++ switch.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int num=2;
+	switch(num)
+	{
+		case 1:printf("One\n");break;
+		case 2:printf("Two\n");
+		case 3:printf("Three\n");break;
+		default:printf("Invalid\n");
+	}
+	return 0;
+}
